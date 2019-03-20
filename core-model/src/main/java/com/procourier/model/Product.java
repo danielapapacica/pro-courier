dff74693a56c63bfac1afee9d121bf79a2d4436d
unknown <dana_papacica@yahoo.ro>
2019-03-20 20:28:01 +0200

    Started core model definition

--- core-model/src/main/java/com/procourier/model/Product.java
+++ core-model/src/main/java/com/procourier/model/Product.java
@@ -1,4 +1,25 @@
 package com.procourier.model;
 
+import java.util.Objects;
+
 public class Product {
+    private Long price;
+    private String name;
+    private String descriere;
+    private Long weight;
+
+    public Product( Long price,
+             String name,
+             String descriere,
+             Long weight){
+        this.price = Objects.requireNonNull(price) ;
+        this.name = Objects.requireNonNull(name);
+        this.descriere = Objects.requireNonNull(descriere);
+        this.weight = Objects.requireNonNull(weight);
+    }
+
+    public static void main(String args[]){
+        final Product product = new Product(12L, null, "ceaaa", 24L);
+    }
+
 }
